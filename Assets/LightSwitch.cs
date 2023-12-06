7a35f0c5399890cdaf678e757654b1a90285d72f
Lmack71 <143634405+Lmack71@users.noreply.github.com>
2023-12-06 12:50:50 -0500

    Update LightSwitch.cs

--- Assets/LightSwitch.cs
+++ Assets/LightSwitch.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    Animiator animator;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
