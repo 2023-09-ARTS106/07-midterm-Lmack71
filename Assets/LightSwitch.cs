091d0ac8713732e350b1ec7306c99c5c4ff1a8be
Lmack71 <143634405+Lmack71@users.noreply.github.com>
2023-12-06 12:49:32 -0500

    Final Game

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    Animiator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("Switch Off", true);
+        }
+    }
+}
